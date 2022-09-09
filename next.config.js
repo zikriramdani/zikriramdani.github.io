f72b64cf6a168f48d265e58b24e293249cfb9cea
ZR <zikriramdani.developer@gmail.com>
2022-09-09 16:36:15 +0700

    app

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? './' : '',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './'
 }
 
 module.exports = nextConfig
