5bec3b0c5f446463c5d4ac74cb62863cbf41fab6
ZR <zikriramdani.developer@gmail.com>
2022-09-09 15:43:40 +0700

    cf

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  assetPrefix: isProd ? 'https://zikriramdani.github.io' : '',
 }
 
 module.exports = nextConfig
