0cca34a3602e569463d4536777eb9fcee4f930d3
ZR <zikriramdani.developer@gmail.com>
2022-09-09 07:15:06 +0700

    bash

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  basePath: 'zikriramdani.github.io',
 }
 
 module.exports = nextConfig
