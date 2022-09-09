2974083682efbe5c1553b922de55483699222a5c
ZR <zikriramdani.developer@gmail.com>
2022-09-09 10:52:07 +0700

    dp

--- next.config.js
+++ next.config.js
@@ -1,31 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const debug = process.env.NODE_ENV !== "production";
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-      "/about": { page: "/about" },
-    }
-  },
-  assetPrefix: !debug ? '' : '',
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
-    config.module.rules = config.module.rules.map(rule => {
-      if(rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
-      }
-      return rule
-    })
-    // Important: return the modified config
-    return config
-  }
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
 
 module.exports = nextConfig
