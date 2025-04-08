c86c2411f47a4408fa2db40c36431f3f8c418b18
zikriramdani <zikriramdani@gmail.com>
2025-04-08 15:33:31 +0700

    NProgress

--- src/redux/provider/index.js
+++ src/redux/provider/index.js
@@ -1,19 +1,39 @@
 // provider.js
 'use client';
-import React, { useRef } from 'react';
+
+import React, { useRef, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { makeStore } from '@/redux/store';
 
 import '../../../i18n';
-import { appWithTranslation } from 'next-i18next';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-function Index({ children }) {
+import { usePathname, useSearchParams } from 'next/navigation';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+NProgress.configure({ showSpinner: false });
+
+export default function ReduxProvider({ children }) {
   const storeRef = useRef();
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
 
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    AOS.init();
+
+    NProgress.start();
+    const timer = setTimeout(() => {
+      NProgress.done();
+    }, 100); // kasih waktu delay biar efeknya terlihat
+
+    return () => clearTimeout(timer);
+  }, [pathname, searchParams]);
+
   return <Provider store={storeRef.current}>{children}</Provider>;
 }
-
-export default appWithTranslation(Index);
