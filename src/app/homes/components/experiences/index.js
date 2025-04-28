4c079045f5e4ab42a5ee9d4922a271e9e72d6995
zikriramdani <zikriramdani@gmail.com>
2025-04-28 21:08:08 +0700

    Fix: Experience

--- src/app/homes/components/experiences/index.js
+++ src/app/homes/components/experiences/index.js
@@ -58,7 +58,13 @@ const Experiences = (props) => {
                           {/* {item.employment_type} -  */}
                           {item.position}
                         </h4>
+<<<<<<< HEAD
                         <div className="c-work__location">{/* {item.location} */}</div>
+=======
+                        <div className="c-work__location">
+                          {/* {item.location} */}
+                        </div>
+>>>>>>> 7dcc91f (Fix: Experience)
                       </div>
                       <div className="o-grid__col-md-7">
                         {/* <h4>Job Desc:</h4> */}
